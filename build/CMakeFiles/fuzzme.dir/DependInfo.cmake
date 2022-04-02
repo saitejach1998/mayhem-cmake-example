49c29195b61f3a490aba24b5afc372c42ed002f9
Saiteja ch <saiteja.ch1998@gmail.com>
2022-04-02 15:40:27 -0400

    Converted into libFuzzer target

---
+++ build/CMakeFiles/fuzzme.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/mayhemuser/hackathon-resources/mayhem-cmake-example/fuzzme.c" "/home/mayhemuser/hackathon-resources/mayhem-cmake-example/build/CMakeFiles/fuzzme.dir/fuzzme.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/fuzzme.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fuzzme.dir/fuzzme.c.o"
+  "fuzzme"
+  "fuzzme.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fuzzme.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
